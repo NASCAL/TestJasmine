eac002bf9a40c4859d832de7e765ebe9278afab8
Nascal <pascal.noliere@orange.fr>
2017-12-05 17:20:20 +0100

    config jenkins

--- src/calc.js
+++ src/calc.js
@@ -8,9 +8,38 @@
 ** La fonction renvoie null en cas d'erreur
 */
 function calculatrice(signe, gauche, droite) {
-    var result = null;
-
-    return result;
+  var result;
+    if (typeof gauche === 'number' && typeof droite === 'number') {
+	   	if (gauche>=0 && droite<430 && droite!=0) {
+			switch (signe) {
+				case "*":
+					result = gauche*droite;
+					return result;
+					break;
+				case "+":
+					result = gauche+droite;
+					return result;
+					break;
+				case "-":
+					result = gauche-droite;
+					return result;
+					break;
+				case "/":
+					result = gauche/droite;
+					return result;
+					break;
+				default:
+					result=null;
+					return result;
+			} 
+		} else {
+		   	result = null;
+		   	return result;
+		}
+	}else{
+		result=null;
+		return result;
+	}
 }
  
 module.exports = calculatrice;
